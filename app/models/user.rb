df4ae11c7932594e3b5a50e04d04a2210e408925
daisuke8000 <daisuke12108000@gmail.com>
2020-07-05 08:04:16 +0900

    passwordのバリデーション明示化

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  #EmailAddressとPassWordはconfig/initializers/devise.rbを参照ください。
+         :recoverable, :rememberable, :validatable, password_length: 8..128
   validates :name,
             length: { minimum: 2, maximum: 20 }
 
