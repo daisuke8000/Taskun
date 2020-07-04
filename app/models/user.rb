dde048d34458017e16e74aebd9f83533abe0b325
daisuke8000 <daisuke12108000@gmail.com>
2020-07-05 06:37:04 +0900

    コメント追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #EmailAddressとPassWordはconfig/initializers/devise.rbを参照ください。
   validates :name,
             length: { minimum: 2, maximum: 20 }
+
 end
