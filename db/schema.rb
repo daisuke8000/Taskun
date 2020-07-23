6c0c6f88bec915f7ada81d2e95fee69d0e0a5692
Eita Aso <muscle03s@gmail.com>
2020-07-23 19:19:47 +0900

    CSS命名の修正

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2020_07_11_035406) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
-ActiveRecord::Schema.define(version: 2020_07_04_020011) do
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
