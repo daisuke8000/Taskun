9689550b4b6c17e94eddeb86b0bd263bb00ef1b2
takuyakawase <takuya86takuya@gmail.com>
2020-08-12 23:26:52 +0900

    Fix:カテゴリー新規登録機能追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ApplicationRecord
   has_many :tasks
+
+  def self.search(search)
+    if search
+      where(['title LIKE ?', "%#{search}%"])
+    else
+      Category.all
+    end
+  end
 end
