af312d4fc9afddf9379b1673e06417b835814123
takatokotan <hiyori0701@yahoo.co.jp>
2020-08-16 16:18:05 +0900

    マークアップ完了

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'tasks/new' => 'tasks#new'
   devise_for :users
   root 'homes#index'
+  get 'users/member' => 'users#member'
   resources :users
   resources :tasks, only: [:index]
   resources :categories
